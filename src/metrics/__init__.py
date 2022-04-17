784323c3621603f1df87836d2ffb94c2374725af
EliasSchaut <eschaut@outlook.de>
2022-04-18 00:59:29 +0200

    add MAPS metric

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -2,16 +2,19 @@ __all__ = (
     'init',
     'update_all',
     '_METRIC',
-    'KD'
+    'KD',
+    'MAPS'
 )
 
 from src.metrics.KD import KD
+from src.metrics.MAPS import MAPS
 
 metrics = []
 
 
 def init():
     metrics.append(KD())
+    metrics.append(MAPS())
 
 
 def update_all(api_data):
