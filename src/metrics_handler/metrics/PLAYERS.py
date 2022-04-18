f2dc97141965605d80e906d14c0ad3aca0b2be94
EliasSchaut <eschaut@outlook.de>
2022-04-18 17:43:34 +0200

    integrate kd and kda metric into player metric

--- src/metrics_handler/metrics/PLAYERS.py
+++ src/metrics_handler/metrics/PLAYERS.py
@@ -4,17 +4,21 @@ from src.metrics_handler._METRIC import _METRIC
 
 class PLAYERS(_METRIC):
     def __init__(self):
-        super().__init__(Gauge('players', 'Total wins on each map', ['name', 'kda_type']))
+        super().__init__(Gauge('players', 'Specific stats (kills, assists, deaths, kd, kda) about all players', ['player_name', 'stat_type']))
 
     def update(self, api_data):
         for player_name in api_data['data']['users']:
             kills = api_data['data']['users'][player_name]['kills']
             assists = api_data['data']['users'][player_name]['assists']
             deaths = api_data['data']['users'][player_name]['deaths']
+            kd = api_data['data']['users'][player_name]['kd']
+            kda = api_data['data']['users'][player_name]['kda']
 
             try:
                 self.METRIC.labels(player_name, 'kills').set(kills)
                 self.METRIC.labels(player_name, 'assists').set(assists)
                 self.METRIC.labels(player_name, 'deaths').set(deaths)
+                self.METRIC.labels(player_name, 'kd').set(deaths)
+                self.METRIC.labels(player_name, 'kda').set(deaths)
             except:
                 continue
--- src/metrics_handler/metrics/__init__.py
+++ src/metrics_handler/metrics/__init__.py
@@ -1,6 +1,4 @@
 __all__ = (
-    'KD',
-    'KDA',
     'MAP_WINS',
     'MAPS',
     'PLAYERS',
