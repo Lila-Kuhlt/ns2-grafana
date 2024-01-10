b073c52425d78d08faace73b2026543ef8cc66ec
EliasSchaut <eschaut@outlook.de>
2024-01-11 00:32:00 +0100

    add more player metrics

--- src/metrics_handler/metrics/PLAYERS.py
+++ src/metrics_handler/metrics/PLAYERS.py
@@ -4,10 +4,13 @@ from metrics_handler._METRIC import _METRIC
 
 class PLAYERS(_METRIC):
     def __init__(self):
-        super().__init__(Gauge('ns2_players', 'Specific stats (kills, assists, deaths, kd, kda, commander) about all players', ['player_name', 'stat_type']))
+        super().__init__(Gauge('ns2_players', 'Specific stats (total_games, marines, aliens, kills, assists, deaths, kd, kda, commander) about all players', ['player_name', 'stat_type']))
 
     def update(self, api_data):
         for player_name in api_data['data']['users']:
+            total_games = api_data['data']['users'][player_name]['total_games']
+            marine_games = api_data['data']['users'][player_name]['marines']
+            alien_games = api_data['data']['users'][player_name]['aliens']
             kills = api_data['data']['users'][player_name]['kills']
             assists = api_data['data']['users'][player_name]['assists']
             deaths = api_data['data']['users'][player_name]['deaths']
@@ -16,6 +19,9 @@ class PLAYERS(_METRIC):
             kda = (kills + assists) / deaths if deaths > 0 else (kills + assists)
 
             try:
+                self.METRIC.labels(player_name, 'total_games').set(total_games)
+                self.METRIC.labels(player_name, 'marine_games').set(marine_games)
+                self.METRIC.labels(player_name, 'alien_games').set(alien_games)
                 self.METRIC.labels(player_name, 'kills').set(kills)
                 self.METRIC.labels(player_name, 'assists').set(assists)
                 self.METRIC.labels(player_name, 'deaths').set(deaths)
